69fe6bdb1358ecdf34e21ca657e9e6fef86a944a
Siddhartha335 <siddhartharaut33@gmail.com>
2024-12-26 21:59:04 +0545

    Practicing for board

---
+++ Unit-1/src/Practice/Practice3.java
@@ -0,0 +1,68 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Practice3 {
+
+//    public int getArea() {
+//        return 0;
+//    }
+//
+//    public int getArea(int length) {
+//        return 4 * length;
+//    }
+//
+//    public int getArea( int len, int bre) {
+//        return len * bre;
+//    }
+
+    public static void main(String[] args) {
+//        int count, i=0;
+//        String str;
+//        count=args.length;
+//        System.out.println("Number of arguments "+count);
+//        while(i<count){
+//            str=args[i];
+//            i=i+1;
+//            System.out.println(i + "Java is "+str+"!\n");
+//        }
+//        Scanner scanner  = new Scanner(System.in);
+//        int[] arr = new int[5];
+//        for(int i=0;i<arr.length;i++) {
+//            System.out.println(String.format("Enter the  %dst element",i));
+//            arr[i] = scanner.nextInt();
+//        }
+//        System.out.println("Printing an array:");
+//        for(int i:arr) {
+//            System.out.print(i + "\t");
+//        }
+        //array multiplication
+//    int[][] arr1 = {{1,2},{3,4}};
+//    int[][] arr2 = {{5,6},{7,8}};
+//
+//    int[][] arr3 = new int[2][2];
+//
+//    for(int i=0; i<arr1.length;i++) {
+//        for(int j=0;j<arr2.length;j++) {
+//            for(int k=0;k<arr3.length;k++) {
+//                arr3[i][j] += arr1[i][k] * arr2[k][j];
+//            }
+//        }
+//    }
+//
+//    for(int[] i:arr3) {
+//        for(int j:i) {
+//            System.out.print(j + "\t");
+//        }
+//        System.out.println();
+//    }
+
+//        Practice3 prac = new Practice3();
+//        System.out.println(prac.getArea());
+//        System.out.println(prac.getArea(5));
+//        System.out.println(prac.getArea(6,5));
+
+
+
+    }
+}
